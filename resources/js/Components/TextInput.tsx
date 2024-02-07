9edeb56561d1a1aa54a605827cc985036c141b09
Joshua Anderton <info@joshanderton.com>
2024-02-06 21:01:27 -0800

    Support password hide/show for TextInput component

--- resources/js/Components/TextInput.tsx
+++ resources/js/Components/TextInput.tsx
@@ -1,5 +1,6 @@
-import React, { forwardRef } from 'react'
+import React, { useState, forwardRef } from 'react'
 import classNames from 'classnames'
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline'
 
 const TextInput = forwardRef<
   HTMLInputElement,
@@ -7,12 +8,33 @@ const TextInput = forwardRef<
     React.InputHTMLAttributes<HTMLInputElement>,
     HTMLInputElement
   >
->((props, ref) => (
-  <input
-    {...props}
-    ref={ref}
-    className={classNames('site-input', props.className)}
-  />
-));
+>(({ type, ...props }, ref) => {
+
+  const [showPassword, setShowPassword] = useState(false)
+
+  return (
+    <div className="relative">
+      <input
+        type={type === 'password' && showPassword ? 'text' : type}
+        {...props}
+        ref={ref}
+        className={classNames('site-input', type === 'password' ? 'pr-10' : '', props.className)}
+      />
+      {type === 'password' && (
+        <button
+          type="button"
+          className="h-full absolute right-0 inset-y-0 px-3 flex items-center hover:opacity-50 transition-opacity"
+          onClick={() => setShowPassword(!showPassword)}
+        >
+          {showPassword ? (
+            <EyeSlashIcon className="w-5 h-auto" strokeWidth={2} />
+          ) : (
+            <EyeIcon className="w-5 h-auto" strokeWidth={2} />
+          )}
+        </button>
+      )}
+    </div>
+  )
+})
 
 export default TextInput
