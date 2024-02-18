8d42a14ba4a799a503d2edf4f45a0aa07de128ac
Joshua Anderton <info@joshanderton.com>
2024-02-17 21:27:21 -0800

    Tweak styles for toggle

--- resources/js/Components/Toggle.tsx
+++ resources/js/Components/Toggle.tsx
@@ -24,12 +24,12 @@ const Toggle: React.FC<Props> = ({ enabled, onChange, label, disabled = false, c
         checked={isEnabled}
         onChange={setIsEnabled}
         className={`${isEnabled ? 'bg-primary' : 'site-bg dark:bg-chrome-700'}
-        ring-1 ring-chrome-400 dark:ring-transparent relative inline-flex h-[30px] w-[56px] shrink-0 cursor-pointer rounded-full border-[3px] border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
+        ring-1 ring-chrome-600 dark:ring-transparent relative inline-flex h-[30px] w-[56px] shrink-0 cursor-pointer rounded-full border-[3px] border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
       >
         <span
           aria-hidden="true"
           className={`${isEnabled ? 'translate-x-[26px]' : 'translate-x-0'}
-            pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full site-bg border site-border dark:bg-chrome-950 shadow-lg ring-0 transition duration-200 ease-in-out`}
+            pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full site-bg border site-border dark:bg-chrome-950 ring-0 transition duration-200 ease-in-out`}
         />
       </Switch>
 
